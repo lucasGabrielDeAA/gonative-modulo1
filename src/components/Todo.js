fbc298712c9bf0b874a8a949bebdb8e5e631cad4
lucasGabrielDeAA <lg.technus@gmail.com>
2018-02-12 16:23:52 -0200

    Stateless component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
     View, Text, StyleSheet
@@ -8,23 +8,19 @@ const styles = StyleSheet.create({
 
 });
 
-class Todo extends Component {
-    static defaultProps = {
-        title: 'Todo standard title'
-    };
-    static propTypes = {
-        //Define the type of the property
-        //To test this functionalitty, you just need to call a Todo Component, passing a title that's is not of the type stringtitle que não seja do tipo string. E.g.: <Todo title={5} />
-        title: PropTypes.string.isRequired
-    };
-    
-    render() {
-        return (
-            <View>
-                <Text>{this.props.title}</Text>
-            </View>
-        );
-    }
-}
+const Todo = ({ title }) => (
+    <View>
+        <Text>{title}</Text>
+    </View>
+);
+
+Todo.defaultProps = {
+    title: 'Todo standard title'
+};
+Todo.propTypes = {
+    //Define the type of the property
+    //To test this functionalitty, you just need to call a Todo Component, passing a title that's is not of the type stringtitle que não seja do tipo string. E.g.: <Todo title={5} />
+    title: PropTypes.string.isRequired
+};
 
 export default Todo;
